f3093a67177f62ea5b1802db37a09a14cc8809ce
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-01-24 19:48:47 +0100

    frontend: handle weird URLs
    show an Angular component for disallowed URLs, see:
    https://angular.io/guide/router#404-page-how-to

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // This should be last:
+  { path: '**', component: PageNotFoundComponent, },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
