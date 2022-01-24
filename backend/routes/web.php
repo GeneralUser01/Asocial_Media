1e0a5f9fd088bb856275c90ff0c1dd3c257b9313
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-01-24 19:47:01 +0100

    serve frontend files from laravel backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Any URL that doesn't go to our backend API should just see the Angular
+// frontend. Note that any files in the public folder will be served first if
+// the URL matches any of their names. So the rules are:
+// 1. Look for public file, if it exists then return that.
+// 2. Look for an API Route in api.php
+// 3. If URL starts with /api then it was supposed to use our API so return "NOT FOUND" (404) error
+// 4. Otherwise this URL is probably used by the Angular frontend so return that and leave the error handling to JavaScript.
+
+Route::any('/api', function() {
+    abort(404);
+});
+Route::any('/api/{any}', function() {
+    abort(404);
+});
+
+Route::fallback(function () {
+    return File::get(public_path() . '/angular-assets/index.html');
 });
