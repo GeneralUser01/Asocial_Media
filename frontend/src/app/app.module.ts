00916cc9f57ae88db26d1f4e85f40b9990bebaeb
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-01-24 19:50:45 +0100

    frontend: fix issue where URL is always prefixed with angular file location.
    The Angular JavaScript re-routed URLs like "/simple" to "/angular-assets/simple" because of our "baseHref" value. With a specified APP_BASE_HREF this is fixed.
    
    See:
    https://angular.io/guide/deployment#the-deploy-url

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
@@ -14,7 +15,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    // Specify the "root" path as understood by our routing (without this the
+    // URL would always be prefixed with the value of `baseHref` inside the
+    // `angular.json` file):
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
