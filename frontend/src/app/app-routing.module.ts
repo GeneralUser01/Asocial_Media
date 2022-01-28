0638cdad490c7af2acc2eea5f61c9a6738dedcd3
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-01-28 19:21:29 +0100

    frontend: worked on login UI

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ShowThreadComponent } from './show-thread/show-thread.component';
 import { ThreadListComponent } from './thread-list/thread-list.component';
@@ -8,6 +9,7 @@ import { UserDetailsComponent } from './user-details/user-details.component';
 
 const routes: Routes = [
   { path: '', component: ThreadListComponent, },
+  { path: 'register', component: RegisterComponent, },
   { path: 'login', component: LoginComponent, },
   { path: 'thread/:threadId', component: ShowThreadComponent, },
   { path: 'user/:username', component: UserDetailsComponent, },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { APP_BASE_HREF } from '@angular/common';
@@ -9,6 +10,8 @@ import { LoginComponent } from './login/login.component';
 import { ThreadListComponent } from './thread-list/thread-list.component';
 import { ShowThreadComponent } from './show-thread/show-thread.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +20,14 @@ import { UserDetailsComponent } from './user-details/user-details.component';
     LoginComponent,
     ThreadListComponent,
     ShowThreadComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     // Specify the "root" path as understood by our routing (without this the
