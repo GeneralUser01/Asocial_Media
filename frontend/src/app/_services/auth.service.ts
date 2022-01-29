f36405f4986a04e0018469d79264ac5b0d924c0d
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-01-29 19:50:19 +0100

    frontend: use real backend's auth routes

--- frontend/src/app/_services/auth.service.ts
+++ frontend/src/app/_services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { from, Observable } from 'rxjs';
+import { from, mergeMap, Observable } from 'rxjs';
 
-const AUTH_API = '/api/auth/';
+const AUTH_API = '/api/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -15,20 +15,37 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  /** Get a new token from the server that uniquely identifies this session.
+   * This token also protects against cross-site request forgery. */
+  getCsrfToken() {
+    return this.http.get('/sanctum/csrf-cookie');
+  }
+
   login(username: string, password: string): Observable<any> {
-   // return from(["not done yet"]);
-    return this.http.post(AUTH_API + 'signin', {
-      username,
-      password
-    }, httpOptions);
+    return this.getCsrfToken()
+      .pipe(mergeMap(() => {
+        // return from(["not done yet"]);
+        return this.http.post(AUTH_API + 'login', {
+          name: username,
+          password
+        }, httpOptions);
+      }))
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'logout', {}, httpOptions);
   }
 
   register(username: string, email: string, password: string): Observable<any> {
-    //return from(["not done yet"]);
-    return this.http.post(AUTH_API + 'signup', {
-      username,
-      email,
-      password
-    }, httpOptions);
+    return this.getCsrfToken()
+      .pipe(mergeMap(() => {
+        //return from(["not done yet"]);
+        return this.http.post(AUTH_API + 'register', {
+          name: username,
+          email,
+          password,
+          password_confirmation: password,
+        }, httpOptions);
+      }))
   }
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './_services/auth.service';
 import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
@@ -15,7 +16,7 @@ export class AppComponent {
   showModeratorBoard = false;
   username?: string;
 
-  constructor(private tokenStorageService: TokenStorageService) { }
+  constructor(private tokenStorageService: TokenStorageService, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.isLoggedIn = !!this.tokenStorageService.getToken();
@@ -32,7 +33,15 @@ export class AppComponent {
   }
 
   logout(): void {
-    this.tokenStorageService.signOut();
-    window.location.reload();
+    const done = () => {
+      this.tokenStorageService.signOut();
+      window.location.reload();
+    };
+    this.authService.logout().subscribe({
+      next: done, error: (error) => {
+        console.error('Logout failed: ', error);
+        done();
+      },
+    });
   }
 }
