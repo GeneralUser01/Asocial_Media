07db33f0d7a7ed93a57edb5b7533740b0d7baad3
Erik <erik.pehrsson@elev.ga.ntig.se>
2022-01-31 14:41:54 +0100

    Began work on post creation component

--- frontend/src/app/post-creation/post-creation.component.ts
+++ frontend/src/app/post-creation/post-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-post-creation',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-creation.component.css']
 })
 export class PostCreationComponent implements OnInit {
+  form: any = {
+    title: null,
+    content: null
+  };
+  isSuccessful = false;
+  isSubmitPostFailed = false;
+  errorMessage = '';
+  serverErrors = { title: null, content: null };
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    // Forget previous server errors:
+    this.serverErrors = { title: null, content: null };
+    this.errorMessage = '';
+    this.isSubmitPostFailed = false;
+
+    const { title, content } = this.form;
+
+    this.httpClient.post(title, content).subscribe({
+      next: (data) => {
+        console.log('Post uploaded successfully: ', data);
+        this.isSuccessful = true;
+        this.isSubmitPostFailed = false;
+      },
+      error: (err) => {
+        console.log('Post upload failed: ', err);
+        this.errorMessage = err.error.message;
+        const errors = err.error.errors;
+        if (errors) {
+          this.serverErrors.title = errors.name || null;
+          this.serverErrors.content = errors.content || null;
+        }
+
+        this.isSubmitPostFailed = true;
+      }
+    });
+  }
 }
