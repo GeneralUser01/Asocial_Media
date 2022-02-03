4d7fbb0403cb1ae7a8aa109040e158a8ff059817
Erik <erik.pehrsson@elev.ga.ntig.se>
2022-02-03 15:41:29 +0100

    Added some code for an alternative in implementing pagination

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+// import { MatPaginatorModule } from '@angular/material/paginator';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -31,6 +32,7 @@ import { PostCreationComponent } from './post-creation/post-creation.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    // MatPaginatorModule,
     HttpClientModule
   ],
   providers: [
