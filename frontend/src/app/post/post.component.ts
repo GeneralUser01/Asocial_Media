c54797670260f5367c6323c62f40dbcc077c429c
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-02-08 02:43:19 +0100

    frontend: Implemented post comments

--- frontend/src/app/post/post.component.ts
+++ frontend/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { catchError, of } from 'rxjs';
 import { PostComment, PostCommentService } from '../_services/post-comment.service';
@@ -7,12 +8,27 @@ import { Post, PostService } from '../_services/post.service';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css', '../app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PostComponent implements OnInit {
   post: null | Post = null;
-  comments: PostComment[] = [];
+  postId: null | string = null;
+  image: null | any = null;
   isLoading = true;
+
+  postComments: PostComment[] = [];
+
+  @ViewChild('content') textarea!: NgForm;
+
+  createdPostComment: null | any = null;
+  form: any = {
+    content: null,
+  };
+  isSuccessful = false;
+  isSubmitCommentFailed = false;
+  errorMessage = '';
+  serverErrors = { content: null };
 
   constructor(
     private postService: PostService,
@@ -23,6 +39,7 @@ export class PostComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe(params => {
       const id = params.get('postId');
+      this.postId = id;
       if (!id) {
         this.post = null;
         this.isLoading = false;
@@ -36,12 +53,55 @@ export class PostComponent implements OnInit {
             this.post = result;
             this.isLoading = false;
           });
-        this.commentService.getComments(id)
-          // Use empty array in case of errors:
-          .pipe(catchError((error) => of([])))
-          .subscribe((comments) => this.comments = comments)
+
+        this.image = 'api/posts/' + id + '/image';
+
+        this.updateComments();
       }
     })
   }
 
+  updateComments() {
+    if (this.postId === null) return;
+
+    this.commentService.getComments(this.postId)
+      // Use empty array in case of errors:
+      .pipe(catchError((error) => of([])))
+      .subscribe((comments) => this.postComments = comments);
+  }
+
+  onSubmit(): void {
+    // Forget previous server errors:
+    this.serverErrors = { content: null };
+    this.errorMessage = '';
+    this.isSubmitCommentFailed = false;
+
+    const { content } = this.form;
+    if (!this.postId) {
+      this.post = null;
+    } else {
+      this.commentService.createComment(this.postId, content).subscribe({
+        next: (data) => {
+          console.log('Post comment uploaded successfully: ', data);
+          this.isSuccessful = true;
+          this.isSubmitCommentFailed = false;
+
+          // Clear comment textarea:
+          this.textarea.reset();
+
+          this.updateComments();
+        },
+        error: (err) => {
+          console.log('Post comment upload failed: ', err);
+          this.errorMessage = err.error.message;
+          const errors = err.error.errors;
+          if (errors) {
+            this.serverErrors.content = errors.content || null;
+          }
+
+          this.isSubmitCommentFailed = true;
+        }
+      });
+    }
+  }
 }
