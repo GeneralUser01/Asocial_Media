8e32274a413ba356d44b150f02da9e253438da81
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-02-08 02:48:10 +0100

    frontend: Implemented selection, loading and uploading of images

--- frontend/src/app/post-creation/post-creation.component.ts
+++ frontend/src/app/post-creation/post-creation.component.ts
@@ -1,53 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { PostService } from '../_services/post.service';
-import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-post-creation',
   templateUrl: './post-creation.component.html',
-  styleUrls: ['./post-creation.component.css']
+  styleUrls: ['./post-creation.component.css', '../app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class PostCreationComponent implements OnInit {
   form: any = {
     title: null,
     body: null,
-    image: null
   };
   isSuccessful = false;
   isSubmitPostFailed = false;
   errorMessage = '';
   serverErrors = { title: null, body: null };
 
-  // selectedFile!: File;
+  fileLoadedCallback: null | (() => void) = null;
 
-  // onFileChanged(event: { target: { files: File[]; }; }) {
-  //   this.selectedFile = event.target.files[0]
-  // }
-
-  selectedFile: File | null = null;
-  fileName = '';
-
-  fileSelect(event: any) {
-    this.selectedFile = <File>event.target.files[0].name;
-    console.log('Selected file: ' + this.selectedFile);
-  }
-
-  // url: any;
-  // msg = "";
-
-  // let reader = new FileReader();
-  // reader.readAsDataURL(<File>event.target.files[0]);
-
-  // reader.onload = (_event) => {
-  //   this.msg = "";
-  //   this.url = reader.result;
-  // }
-
-  constructor(private http: HttpClient, private postService: PostService) { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
   }
+
+  selectedFile: null | File = null;
+  fileData: null | ArrayBuffer = null;
+  fileSelect(event: any) {
+    this.selectedFile = event.target.files[0] as File;
+    let myReader = new FileReader();
+    // let fileType = inputValue.parentElement.id;
+
+    myReader.onloadend = () => {
+      this.fileData = myReader.result as ArrayBuffer;
+
+      console.log('Loaded file as ArrayBuffer: ' + this.fileData);
+      if (this.fileLoadedCallback !== null) {
+        this.fileLoadedCallback();
+        this.fileLoadedCallback = null;
+      }
+    };
+
+    myReader.readAsArrayBuffer(this.selectedFile);
+  }
+
 
   onSubmit(): void {
     // Forget previous server errors:
@@ -56,35 +53,36 @@ export class PostCreationComponent implements OnInit {
     this.isSubmitPostFailed = false;
 
     const { title, body } = this.form;
-    this.selectedFile = this.form
-    // let image = null;
-    // if (this.selectedFile !== null) {
-    //   image = new FormData().append('image', this.selectedFile, this.selectedFile.name);
-    // }
 
-    this.postService.addPost(title, body, this.selectedFile).subscribe({
-      next: (data) => {
-        console.log('Post uploaded successfully: ', data);
-        this.isSuccessful = true;
-        this.isSubmitPostFailed = false;
-      },
-      error: (err) => {
-        console.log('Post upload failed: ', err);
-        this.errorMessage = err.error.message;
-        const errors = err.error.errors;
-        if (errors) {
-          this.serverErrors.title = errors.title || null;
-          this.serverErrors.body = errors.body || null;
-        }
-
-        this.isSubmitPostFailed = true;
+    const doWork = () => {
+      let imageData = null;
+      if (this.fileData !== null && this.selectedFile !== null) {
+        imageData = new Blob([this.fileData], { type: this.selectedFile.type });
       }
-    });
-    //   if (this.selectedFile) {
-    //   const formData = new FormData();
-    //   formData.append("image", this.selectedFile);
-    //   const upload$ = this.http.post("../api/posts/", formData);
-    //   upload$.subscribe();
-    // }
+      this.postService.createPost(title, body, imageData).subscribe({
+        next: (data) => {
+          console.log('Post uploaded successfully: ', data);
+          this.isSuccessful = true;
+          this.isSubmitPostFailed = false;
+        },
+        error: (err) => {
+          console.log('Post upload failed: ', err);
+          this.errorMessage = err.error.message;
+          const errors = err.error.errors;
+          if (errors) {
+            this.serverErrors.title = errors.title || null;
+            this.serverErrors.body = errors.body || null;
+          }
+
+          this.isSubmitPostFailed = true;
+        }
+      });
+    };
+    if (this.selectedFile !== null && this.fileData === null) {
+      // Have selected a file and that file is still loading, so wait:
+      this.fileLoadedCallback = doWork;
+    } else {
+      doWork();
+    }
   }
 }
