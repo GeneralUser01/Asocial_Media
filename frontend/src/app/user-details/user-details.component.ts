69abeea099db5734e2bd1ceb90dd110ca433df52
Erik <90757653+GeneralUser01@users.noreply.github.com>
2022-02-16 03:55:46 +0100

    frontend: Implemented functionality to view user details and make admin, disable or delete an account

--- frontend/src/app/user-details/user-details.component.ts
+++ frontend/src/app/user-details/user-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { CurrentUser, UserService } from '../_services/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { EMPTY, mergeMap, tap } from 'rxjs';
+import { RolesService, WithRolesInfo } from '../_services/roles.service';
+import { CurrentUser, User, UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -7,13 +10,69 @@ import { CurrentUser, UserService } from '../_services/user.service';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-  currentUser: CurrentUser | null = null;
-  constructor(private userService: UserService) { }
+  currentUser: (CurrentUser & Partial<WithRolesInfo>) | null = null;
+
+  user: (User & Partial<WithRolesInfo>) | null = null;
+  userId: null | string = null;
+
+  constructor(
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private rolesService: RolesService
+  ) { }
 
   ngOnInit(): void {
-    this.userService.getCurrentUser().subscribe(user => {
-      this.currentUser = user;
+    this.userService.getCurrentUser().pipe(
+      tap(user => this.currentUser = user),
+      mergeMap(user => {
+        if (!user) return EMPTY;
+        return this.rolesService.getRolesInfo(user);
+      }),
+    ).subscribe();
+
+
+    this.activatedRoute.paramMap.subscribe(params => {
+      const id = params.get('userId');
+      if (!id) return;
+      this.userId = id;
+      this.updateUser();
     });
   }
 
+  updateUser() {
+    if (this.userId === null) return;
+    this.userService.getUser(this.userId).pipe(
+      tap(user => this.user = user),
+      mergeMap(user => {
+        if (!user) return EMPTY;
+        return this.rolesService.getRolesInfo(user);
+      }),
+    ).subscribe();
+  }
+
+  toggleDisabled() {
+    if (this.userId === null) return;
+    // TODO: actually get the real disabled id.
+    const disabledId = 2;
+    if (this.user?.is_disabled) {
+      this.rolesService.removeRoleFromUser(disabledId, this.userId).subscribe(() => this.updateUser());
+    } else {
+      this.rolesService.addRoleToUser(disabledId, this.userId).subscribe(() => this.updateUser());
+    }
+  }
+
+  toggleAdmin() {
+    if (this.userId === null) return;
+    // TODO: actually get the real disabled id.
+    const adminId = 1;
+    if (this.user?.is_admin) {
+      this.rolesService.removeRoleFromUser(adminId, this.userId).subscribe(() => this.updateUser());
+    } else {
+      this.rolesService.addRoleToUser(adminId, this.userId).subscribe(() => this.updateUser());
+    }
+  }
+  deleteUser() {
+    if (this.userId === null) return;
+    this.userService.deleteUser(this.userId).subscribe(() => this.user = null);
+  }
 }
